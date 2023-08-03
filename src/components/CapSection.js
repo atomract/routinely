7df19cd3d1f827df5312129fbd663c4c33cec4f3
Siddhant Aggarwal <46929915+atomract@users.noreply.github.com>
2023-08-04 02:54:09 +0530

    //

--- src/components/CapSection.js
+++ src/components/CapSection.js
@@ -148,7 +148,7 @@ const CapsuleSection = () => {
       <div className=''>
           <div className='place-content-center'>
             <div className='contentBox'>
-            <img className='eclip sm:p-28 md:p-32 lg:p-40 xl:p-64 2xl:p-72 mt-14' />
+            <img className='eclip sm:p-28 md:p-32 lg:p-40 xl:p-52 2xl:p-64 mt-14' />
             {/* <div onMouseMove={handleMouseMove} style={{
                 backgroundPositionX: bgX
               }}> */}
@@ -167,7 +167,7 @@ const CapsuleSection = () => {
 
                 </div>
 
-                <AirPodsAnimation className='mb-48' />
+                <AirPodsAnimation />
 
               {/* <div ref={setHeightRef} id="set-height">
               </div> */}
